30e54369a1169961d096975134b4c9ee5f95029b
Junior de Sá <juniorn06@gmail.com>
2022-03-15 00:05:46 -0300

    Interfaces Funcionais #01

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+@FunctionalInterface // define que uma interface funcional pode ter apenas uma única função
 public interface Calculo {
 
     double executar(double a, double b);
